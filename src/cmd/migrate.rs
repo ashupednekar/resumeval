226762845f31d39f899fb9695789ad41fe9761c5
ashupednekar <ashutoshpednekar15@gmail.com>
2025-10-13 13:53:24 +0530

    chore: added schema selection to migrate

--- src/cmd/migrate.rs
+++ src/cmd/migrate.rs
@@ -1,12 +1,14 @@
-use crate::conf::settings;
+use std::sync::Arc;
+
+use crate::{conf::settings, pkg::server::state::GetTxn};
 use sqlx::{PgPool, migrate::Migrator};
 
 static MIGRATOR: Migrator = sqlx::migrate!("./migrations");
 
 pub async fn apply() -> Result<(), sqlx::Error> {
-    let pool = PgPool::connect(&settings.database_url).await?;
-    //sqlx::query(&format!("set search_path to {}", &settings.database_schema)).execute(&pool).await?;
-    MIGRATOR.run(&pool).await?;
+    let pool = Arc::new(PgPool::connect(&settings.database_url).await?);
+    let mut tx = pool.begin_txn().await?;
+    MIGRATOR.run(&mut tx).await?;
     println!("Migrations applied successfully");
     Ok(())
 }
