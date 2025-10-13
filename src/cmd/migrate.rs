d4f38b60cac338adc6095fe6a6f0a641becdfbf4
ashupednekar <ashutoshpednekar15@gmail.com>
2025-10-13 16:19:42 +0530

    fix: added offline migration support

--- src/cmd/migrate.rs
+++ src/cmd/migrate.rs
@@ -1,21 +1,30 @@
 use std::sync::Arc;
 
 use crate::{conf::settings, prelude::Result, pkg::server::state::GetTxn};
-use sqlx::{PgPool, migrate::Migrator};
+use sqlx::{migrate::Migrator, postgres::PgPoolOptions, PgPool};
 use standard_error::{Interpolate, StandardError};
 
 static MIGRATOR: Migrator = sqlx::migrate!("./migrations");
 
+
 pub async fn apply() -> Result<()> {
-    let pool = Arc::new(PgPool::connect(&settings.database_url).await
-        .map_err(|e| StandardError::new("ERR-DB-000").interpolate_err(e.to_string()))
-        ?);
+    let pool = Arc::new(
+        PgPoolOptions::new()
+            .connect(&settings.database_url)
+            .await
+            .map_err(|e| StandardError::new("ERR-DB-000").interpolate_err(e.to_string()))?
+    );
+    
     tracing::debug!("connected to db");
     let mut tx = pool.begin_txn().await?;
     tracing::debug!("set search path and started txn");
-    MIGRATOR.run(&mut tx).await
-        .map_err(|e| StandardError::new("ERR-DB-000").interpolate_err(e.to_string()))
-        ?;
+    
+    MIGRATOR.run(&mut *tx).await
+        .map_err(|e| StandardError::new("ERR-DB-000").interpolate_err(e.to_string()))?;
+    
+    tx.commit().await
+        .map_err(|e| StandardError::new("ERR-DB-000").interpolate_err(e.to_string()))?;
+    
     println!("Migrations applied successfully");
     Ok(())
 }
