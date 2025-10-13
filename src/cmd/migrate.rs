757acd03d481d6b3648d32c52815e413f967e424
ashupednekar <ashutoshpednekar15@gmail.com>
2025-10-13 14:06:41 +0530

    fix: migrate error handling

--- src/cmd/migrate.rs
+++ src/cmd/migrate.rs
@@ -1,14 +1,19 @@
 use std::sync::Arc;
 
-use crate::{conf::settings, pkg::server::state::GetTxn};
+use crate::{conf::settings, prelude::Result, pkg::server::state::GetTxn};
 use sqlx::{PgPool, migrate::Migrator};
+use standard_error::{Interpolate, StandardError};
 
 static MIGRATOR: Migrator = sqlx::migrate!("./migrations");
 
-pub async fn apply() -> Result<(), sqlx::Error> {
-    let pool = Arc::new(PgPool::connect(&settings.database_url).await?);
+pub async fn apply() -> Result<()> {
+    let pool = Arc::new(PgPool::connect(&settings.database_url).await
+        .map_err(|e| StandardError::new("ERR-DB-000").interpolate_err(e.to_string()))
+        ?);
     let mut tx = pool.begin_txn().await?;
-    MIGRATOR.run(&mut tx).await?;
+    MIGRATOR.run(&mut tx).await
+        .map_err(|e| StandardError::new("ERR-DB-000").interpolate_err(e.to_string()))
+        ?;
     println!("Migrations applied successfully");
     Ok(())
 }
